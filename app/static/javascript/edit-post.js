d4ae3ac3875d2b61390355ff7253082ad299b626
Alan Phelps <phelpsa64@gmail.com>
2022-08-05 12:30:46 -0400

    all other API routes created and tested; users can update titles and links for posts

--- app/static/javascript/edit-post.js
+++ app/static/javascript/edit-post.js
@@ -2,13 +2,15 @@ async function editFormHandler(event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="post-title"]').value.trim();
+  const post_url = document.querySelector('input[name="post-url"]').value;
   const id = window.location.toString().split('/')[
     window.location.toString().split('/').length - 1
   ];
   const response = await fetch(`/api/posts/${id}`, {
     method: 'PUT',
     body: JSON.stringify({
-      title
+      title,
+      post_url
     }),
     headers: {
       'Content-Type': 'application/json'
@@ -18,6 +20,7 @@ async function editFormHandler(event) {
   if (response.ok) {
     document.location.replace('/dashboard/');
   } else {
+    console.log(response);
     alert(response.statusText);
   }
 }
